76e7d4a3457db615bd4d2e57b24ef719a8cc179f
myckhel <myckhel1@hotmail.com>
2020-07-27 19:40:42 +0100

    disabled routes

--- config/checkmobi.php
+++ config/checkmobi.php
@@ -2,6 +2,7 @@
 
 return [
   "secret_key"          => env("CHECKMOBI_SECRET_KEY"),
+    /* coming soon */
   "route_middleware"    => null, // For injecting middleware to the package's routes
   "retry_after"         => 36000, // flag verification expiry time in seconds
 ];
