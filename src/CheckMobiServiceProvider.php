f26b8cdc499eb94c01cdd5d839f7294a59754754
MARK LUKOSHKO <36704379+MarkLukoshko@users.noreply.github.com>
2023-03-29 13:10:03 +0200

    Update CheckMobiServiceProvider.php

--- src/CheckMobiServiceProvider.php
+++ src/CheckMobiServiceProvider.php
@@ -40,8 +40,8 @@ class CheckMobiServiceProvider extends ServiceProvider
     {
       // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'myckhel');
       // $this->loadViewsFrom(__DIR__.'/../resources/views', 'myckhel');
-      $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-      $this->loadRoutesFrom(__DIR__.'/routes.php');
+      // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+      // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
       // Publishing is only necessary when using the CLI.
       if ($this->app->runningInConsole()) {
